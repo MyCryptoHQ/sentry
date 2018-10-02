b251b491ba1dea6b0d69caded376522ad633e4bd
Danny Skubak <skubakdj@gmail.com>
2018-10-02 12:49:34 -0400

    adapt to new api

--- utils/showSlackBotInfo.js
+++ utils/showSlackBotInfo.js
@@ -1,28 +1,35 @@
 const path = require('path');
-const configPath = path.resolve(process.argv[2]);
+const configPath = path.resolve(process.env.SENTRY_CONFIG_PATH);
 
-const BOT_TOKEN = require(configPath).slack.apiToken;
-const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-const RtmClient = require('@slack/client').RtmClient;
-const rtm = new RtmClient(BOT_TOKEN);
+if (!configPath.length) {
+  throw new Error('Invalid config path on env var "SENTRY_CONFIG_PATH".');
+}
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, parseDataAndExit);
+const BOT_TOKEN = require(configPath).SLACK_API_TOKEN;
+const { RTMClient, WebClient } = require('@slack/client');
+const rtm = new RTMClient(BOT_TOKEN);
+const web = new WebClient(BOT_TOKEN);
+
+rtm.on('authenticated', parseDataAndExit);
 rtm.start();
 
-function parseDataAndExit(rtmStartData) {
+// web.channels.list().then(parseDataAndExit);
+
+async function parseDataAndExit(rtmStartData) {
   const availableChannels = [];
   const botName = rtmStartData.self.name;
   const botId = rtmStartData.self.id;
+  const { channels } = await web.conversations.list();
+  const { groups } = await web.groups.list();
 
-  rtmStartData.groups.forEach(function(group) {
+  groups.forEach(function(group) {
     availableChannels.push({
       name: group.name,
       id: group.id
     });
   });
 
-  rtmStartData.channels.forEach(function(channel) {
+  channels.forEach(function(channel) {
     if (channel.is_member) {
       availableChannels.push({
         name: channel.name,
